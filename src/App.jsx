6fc2450045e60402514e032d6a077ea8e65ddeaa
Kidyoh <kidyoh789@gmail.com>
2023-09-19 15:52:34 +0300

    Removed unused declarations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 // App.js
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import Dropdown from 'react-dropdown';
-import { HiSwitchHorizontal } from 'react-icons/hi';
 import 'react-dropdown/style.css';
 import './App.css';
 import SelectCountry from './components/selectCountry';
@@ -12,14 +10,10 @@ import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 
 function App() {
 
-  // Initializing all the state variables
-  const [info, setInfo] = useState([]);
   const [input, setInput] = useState(0);
   const [from, setFrom] = useState("ETB - Ethiopia");
   const [to, setTo] = useState("USD - United States");
-  const [options, setOptions] = useState([]);
   const [output, setOutput] = useState(0);
-  const [currencies, setCurrencies] = useState([]);
   const [exchangeRates, setExchangeRates] = useState({});
 
   const boxStyles = {
